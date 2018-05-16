eb8eab423d79803058803d0372cb1dbc30ca8675
Nick Camillo <nick.camillo@gmail.com>
2018-05-16 14:41:09 -0400

    implement calculate_score method

--- test/test_word_scorer.rb
+++ test/test_word_scorer.rb
@@ -11,4 +11,17 @@ class TestWordScorer < Test::Unit::TestCase
     assert_equal(word, word_scorer.word)
     assert_equal(score, word_scorer.score)
   end
+
+  def test_word_scorer_calculate_score
+    word = 'RUBY'
+    word_2 = 'JAVASCRIPT'
+    word_3 = 'PYTHON'
+    word_scorer = WordScorer.new(word)
+    word_scorer_2 = WordScorer.new(word_2)
+    word_scorer_3 = WordScorer.new(word_3)
+
+    assert_equal(9, word_scorer.calculate_score)
+    assert_equal(24, word_scorer_2.calculate_score)
+    assert_equal(14, word_scorer_3.calculate_score)
+  end
 end
